16500c0dc2617e708ff10b8ec2b78d359bef5f6b
mamad33 <mohmohammad679@gmail.com>
2021-02-20 19:06:10 +0330

    login2

---
+++ login2/login1.php
@@ -0,0 +1,81 @@
+<?php
+
+$conn = mysqli_connect("localhost" , "root" , "" , "login2") ;
+
+if (isset($_POST['btn'])){
+
+   $name = $_POST['username'] ;
+   $password = $_POST['password'] ;
+
+
+   $query = "SELECT * FROM  login WHERE email = '$name' " ;
+
+   $row = mysqli_query($conn , $query) ;
+   $res = mysqli_fetch_assoc($row) ;
+
+   if ($res['pass'] == $password){
+    header("location:welcome.php");
+   }
+   else{
+       header("location:login1.php?login1?error");
+   }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iofrm</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-style.css">
+    <link rel="stylesheet" type="text/css" href="css/iofrm-theme1.css">
+</head>
+<body>
+    <div class="form-body" class="container-fluid">
+        <div class="website-logo">
+            <a href="index.html">
+                <div class="logo">
+                    <img class="logo-size" src="images/logo-light.svg" alt="">
+                </div>
+            </a>
+        </div>
+        <div class="row">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="info-holder">
+
+                </div>
+            </div>
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <h3>Get more things done with Loggin platform.</h3>
+                        <p>Access to the most powerfull tool in the entire design and web industry.</p>
+                        <div class="page-links">
+                            <a href="login1.php" class="active">Login</a><a href="register1.php">Register</a>
+                        </div>
+                        <form method="post">
+                            <input class="form-control" type="text" name="username" placeholder="E-mail Address" required>
+                            <input class="form-control" type="password" name="password" placeholder="Password" required>
+                            <div class="form-button">
+                                <button id="submit" type="submit" class="ibtn" name="btn">Login</button>
+                            </div>
+                        </form>
+                        <div class="other-links">
+                            <span>Or login with</span><a href="#">Facebook</a><a href="#">Google</a><a href="#">Linkedin</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/popper.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
